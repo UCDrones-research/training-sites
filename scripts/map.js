a00a6ab085c82f242f198b5d13cad13d32bf8f02
bstark2 <bstark2@ucmerced.edu>
2020-11-18 21:02:09 -0800

    update UI

--- scripts/map.js
+++ scripts/map.js
@@ -1,10 +1,18 @@
+var max_Zoom_Out = 2400000;
+
+//https://developers.arcgis.com/javascript/latest/sample-code/widgets-layerlist-actions/index.html
+
 require([
   "esri/Map",
   "esri/Basemap",
   "esri/views/MapView",
   "esri/layers/FeatureLayer",
   "esri/geometry/Extent",
-], function (Map, Basemap, MapView, FeatureLayer, Extent) {
+  "esri/widgets/Expand",
+  "esri/widgets/LayerList"
+], function (Map, Basemap, MapView, FeatureLayer, Extent, Expand, LayerList) {
+  let siteLayerView;
+	
   var classAirspacerendered = {
     type: "unique-value",
     field: "LOWER_VAL",
@@ -118,7 +126,7 @@ require([
 	 type: "simple",  // autocasts as new SimpleRenderer()
      symbol: {
 		type: "simple-fill",  // autocasts as new SimpleFillSymbol()
-		color: [ 255, 128, 0, 0.1 ],
+		color: [ 255, 128, 0, 0.0 ],
 		outline: {  // autocasts as new SimpleLineSymbol()
 		  width: 2,
 		  color: "white"
@@ -161,10 +169,25 @@ var UASFacilitiesLabels = {
   labelExpressionInfo: {
     expression: "$feature.CEILING" 
   },
-  minScale: 500000,
+  minScale: 400000,
   maxScale: 0,
 };
 
+var NFZ_Renderer = {
+	 type: "simple",  // autocasts as new SimpleRenderer()
+     symbol: {
+		type: "simple-fill",  // autocasts as new SimpleFillSymbol()
+		color: [ 128, 0, 0, 0.6 ],
+		outline: {  // autocasts as new SimpleLineSymbol()
+		  width: 2,
+		  color: "red"
+		}
+	  }
+  };
+
+var TestSite_Renderer = {
+
+};
 
   var classAirspace = new FeatureLayer({
     url:
@@ -174,6 +197,9 @@ var UASFacilitiesLabels = {
     visible: true,
     renderer: classAirspacerendered,
     opacity: 0.8,
+	minScale: max_Zoom_Out,
+	maxScale: 0,
+	title: "FAA Airspace Class",
     definitionExpression:
       "STATE = 'CA' AND (LOCAL_TYPE='CLASS_E2' AND LOWER_VAL=0 OR LOCAL_TYPE='CLASS_B' AND LOWER_VAL=0 OR LOCAL_TYPE='CLASS_C' AND LOWER_VAL=0 OR LOCAL_TYPE='CLASS_D' AND LOWER_VAL=0)",
   });
@@ -183,11 +209,12 @@ var UASFacilitiesLabels = {
       "https://services6.arcgis.com/ssFJjBXIUyZDrSYZ/arcgis/rest/services/FAA_UAS_FacilityMap_Data_V3/FeatureServer/0/query?outFields=*&where=1%3D1",
     outFields: ["*"],
     //popupTemplate: classAirspaceTemplate,
-	minScale: 2400000,
+	minScale: max_Zoom_Out,
 	maxScale: 0,
 	renderer: uasFacilitiesRenderer,
 	labelingInfo: [UASFacilitiesLabels],
     opacity: 0.8,
+	title: "UAS Facility Maps",
     definitionExpression:
       "REGION = 'Western' ",
   });
@@ -196,6 +223,7 @@ var UASFacilitiesLabels = {
     url:
       "https://gisdata.dot.ca.gov/arcgis/rest/services/Boundary/District_Tiger_Lines/MapServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json",
     renderer:DistrictRenderer,
+	title: "Caltrans Districts",
 	labelingInfo: [DistrictLabels]
   });
 
@@ -204,11 +232,32 @@ var UASFacilitiesLabels = {
       "https://services.arcgis.com/BLN4oKB0N1YSgvY8/arcgis/rest/services/Counties_in_California/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json",
     opacity: 0.1,
     color: "red",
+	
   });
 
   var UASTestSite = new FeatureLayer({
-	 url: "https://services2.arcgis.com/wx8u046p68e0iGuj/arcgis/rest/services/UAS_Test_Sites/FeatureServer?token=Lht-1hBd8WuF59T6ujlr3nmiUzd2G6uhXTnrnmDeJYI7sgsDe3BXUhyfyD9syqZvkHipaUaE-Qd2tEmD4s3uxIhf6XgLUkvROK-ToitT5T2Vnnezt9LUh6UDROVlHiyRO3TF0iU6XKdp2ZR2yKzbQE3iBowp4cy3Regw70C8R0lMBXSCbk1ONqUqT5XTuQL7ntcAoBTyJ2FsXoOrNBq7-VvNCOQaQTCafSyXGEVdAgIGYsWW1qo--jMpVITzYU1b" 
+	 url: "https://services2.arcgis.com/wx8u046p68e0iGuj/arcgis/rest/services/UAS_Test_Sites/FeatureServer?token=Lht-1hBd8WuF59T6ujlr3nmiUzd2G6uhXTnrnmDeJYI7sgsDe3BXUhyfyD9syqZvkHipaUaE-Qd2tEmD4s3uxIhf6XgLUkvROK-ToitT5T2Vnnezt9LUh6UDROVlHiyRO3TF0iU6XKdp2ZR2yKzbQE3iBowp4cy3Regw70C8R0lMBXSCbk1ONqUqT5XTuQL7ntcAoBTyJ2FsXoOrNBq7-VvNCOQaQTCafSyXGEVdAgIGYsWW1qo--jMpVITzYU1b",
+     outFields: ["*"],
+	listMode: "hide",
   });
+  
+  var FAA_NS_NFZ = new FeatureLayer({
+	 url:
+	    "https://services6.arcgis.com/ssFJjBXIUyZDrSYZ/arcgis/rest/services/DoD_Mar_13/FeatureServer/0",
+		outFields: ["*"],
+    //popupTemplate: classAirspaceTemplate,
+    visible: true,
+    renderer: NFZ_Renderer,
+    opacity: 0.8,
+	minScale: max_Zoom_Out,
+	maxScale: 0,
+	listMode: "hide",
+    definitionExpression:
+      "STATE = 'CA'",
+	 //renderer: NFZ_Renderer,
+  });
+  
+  
 
   var map = new Map({
     basemap: "satellite",
@@ -217,6 +266,7 @@ var UASFacilitiesLabels = {
       uasFacilities,
       CalTransDistrictBound,
       //CalTransCountyBound,
+	  FAA_NS_NFZ,
 	  UASTestSite,
     ],
   });
@@ -227,4 +277,62 @@ var UASFacilitiesLabels = {
     zoom: 11, // Sets zoom level based on level of detail (LOD)
     center: [-120.420165, 37.363572], // longitude, latitude
   });
+  
+  //const sitesNodes = document.querySelectorAll(`.site-item`);
+  const sitesElement = document.getElementById("sites-filter");
+  
+
+  // click event handler for sites choices
+  sitesElement.addEventListener("click", filterBysite);
+  
+
+  function filterBysite(event) {
+	var par = event.target.parentNode;
+	var c = par.children;
+	var i;
+	for (i = 0; i < c.length; i++) {
+		c[i].classList.remove("active");
+	}
+  
+	event.target.classList.add("active");
+	const selectedsite = event.target.getAttribute("data-site");
+	siteLayerView.filter = {
+		where: selectedsite + " = 1"
+	};
+  };
+  
+
+var layerList = new LayerList({
+  view: view
+});
+// Adds widget below other elements in the top left corner of the view
+view.ui.add(layerList, {
+  position: "top-right"
+});
+  
+
+
+  view.whenLayerView(UASTestSite).then(function (layerView) {
+	// flash flood warnings layer loaded
+	// get a reference to the flood warnings layerview
+	siteLayerView = layerView;
+
+	// set up UI items
+	sitesElement.style.visibility = "visible";
+	const sitesExpand = new Expand({
+		view: view,
+		content: sitesElement,
+		expandIconClass: "esri-icon-filter",
+		group: "top-left"
+	});
+	//clear the filters when user closes the expand widget
+	sitesExpand.watch("expanded", function () {
+		if (!sitesExpand.expanded) {
+			siteLayerView.filter = null;
+		}
+	});
+  view.ui.add(sitesExpand, "top-left");
+  view.ui.add("titleDiv", "top-right");
+  });
+  
 });
