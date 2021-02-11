6302c9f724d6e85fd7160b952a293ee088a79b48
Daniel Alejandro Maciel Manzo <72098517+DanielMacielManzo@users.noreply.github.com>
2021-02-11 09:02:03 -0800

    buttons wired

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,6 +1,45 @@
 /* ################   User Interaction   ################*/
 function actionToggle(i) {
-	console.log(i.target);
+	if (i.target.innerHTML == "unfold_less") {
+		i.target.innerHTML = "unfold_more";
+		switch(i.target.id) {
+			case 'expand_layers':
+				document.getElementById("content_layers").style.display = 'inherit';
+			  break;
+			case 'expand_legend':
+				document.getElementById("content_legend").style.display = 'inherit';
+				break;
+			case 'expand_filter':
+				document.getElementById("content_filter").style.display = 'inherit';
+				break;
+			case 'expand_info':
+				document.getElementById("content_info").style.display = 'inherit';
+				break;
+			default:
+				console.log("no attribute matches listener to class");
+			  // code block
+		  }
+	} else {
+		//if its open close
+		i.target.innerHTML = "unfold_less";
+		switch(i.target.id) {
+			case 'expand_layers':
+				document.getElementById("content_layers").style.display = 'none';
+			  break;
+			case 'expand_legend':
+				document.getElementById("content_legend").style.display = 'none';
+				break;
+			case 'expand_filter':
+				document.getElementById("content_filter").style.display = 'none';
+				break;
+			case 'expand_info':
+				document.getElementById("content_info").style.display = 'none';
+				break;
+			default:
+				console.log("no attribute matches listener to class");
+			  // code block
+		  }
+	}
 }
 
 window.onload = function () {
@@ -18,9 +57,10 @@ window.onload = function () {
 		}
 	});
 
-	var btns = document.getElementsByClassName("cardheader");
-
-	// Add our event listeners
-  window.addEventListener("click", actionToggle, false);
+	document.querySelectorAll('.showmore').forEach(item => {
+		item.addEventListener('click', event => {
+			actionToggle(event);
+		})
+	  })
   
 };
