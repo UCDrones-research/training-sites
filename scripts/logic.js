57311151c6b5d845b7d5b566dd62e7d790e70faa
Daniel Alejandro Maciel Manzo <72098517+DanielMacielManzo@users.noreply.github.com>
2021-02-11 11:57:52 -0800

    breakpoints still need some work
    fix the screen media breakpoints, for smoother transitions

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,47 +1,64 @@
 /* ################   User Interaction   ################*/
+function isBreakpoint() {
+	var w = window.innerWidth;
+	var h = window.innerHeight;
+
+	if (w <= 768) {
+		if (h <= 544) {
+			//mobile landscape
+			return 0;
+		} else {
+			//mobile portrait
+			return 1;
+		}
+	} else if (h >= 544) {
+		return 2;
+	}
+}
+
 function actionToggle(i) {
 	if (i.target.innerHTML == "unfold_more") {
 		i.target.innerHTML = "unfold_less";
-		switch(i.target.id) {
-			case 'expand_layers':
-				document.getElementById("content_layers").style.display = 'inherit';
-			  break;
-			case 'expand_legend':
-				document.getElementById("content_legend").style.display = 'inherit';
+		switch (i.target.id) {
+			case "expand_layers":
+				document.getElementById("content_layers").style.display = "inherit";
 				break;
-			case 'expand_filter':
-				document.getElementById("content_filter").style.display = 'inherit';
-				document.getElementById("sites-filter").style.display = 'inherit';
+			case "expand_legend":
+				document.getElementById("content_legend").style.display = "inherit";
+				break;
+			case "expand_filter":
+				document.getElementById("content_filter").style.display = "inherit";
+				document.getElementById("sites-filter").style.display = "inherit";
 
 				break;
-			case 'expand_info':
-				document.getElementById("content_info").style.display = 'inherit';
+			case "expand_info":
+				document.getElementById("content_info").style.display = "inherit";
 				break;
 			default:
 				console.log("no attribute matches listener to class");
-			  // code block
-		  }
+			// code block
+		}
 	} else {
 		//if its open close
 		i.target.innerHTML = "unfold_more";
-		switch(i.target.id) {
-			case 'expand_layers':
-				document.getElementById("content_layers").style.display = 'none';
-			  break;
-			case 'expand_legend':
-				document.getElementById("content_legend").style.display = 'none';
+		switch (i.target.id) {
+			case "expand_layers":
+				document.getElementById("content_layers").style.display = "none";
 				break;
-			case 'expand_filter':
-				document.getElementById("content_filter").style.display = 'none';
-				document.getElementById("sites-filter").style.display = 'none';
+			case "expand_legend":
+				document.getElementById("content_legend").style.display = "none";
 				break;
-			case 'expand_info':
-				document.getElementById("content_info").style.display = 'none';
+			case "expand_filter":
+				document.getElementById("content_filter").style.display = "none";
+				document.getElementById("sites-filter").style.display = "none";
+				break;
+			case "expand_info":
+				document.getElementById("content_info").style.display = "none";
 				break;
 			default:
 				console.log("no attribute matches listener to class");
-			  // code block
-		  }
+			// code block
+		}
 	}
 }
 
@@ -50,21 +67,36 @@ window.onload = function () {
 	var menuAction = document.getElementById("actionmenu");
 	var gridLayout = document.getElementById("main");
 	menuAction.addEventListener("click", function () {
+		var x = isBreakpoint();
+		console.log(x);
 		if (this.innerHTML == "close") {
 			this.innerHTML = "menu_open";
-			gridLayout.style.gridTemplateRows = "60px auto 0%";
-			dri
+			if (x == 0) {
+				gridLayout.style.gridTemplateRows = "40px auto auto";
+				gridLayout.style.gridTemplateColumns = "0 auto";
+			} else if (x == 1) {
+				//mobile landscape
+				gridLayout.style.gridTemplateRows = "60px auto 0%";
+				gridLayout.style.gridTemplateColumns = "60px auto 0%";
+			} else if (x == 2) {
+			}
 		} else {
-			//if its open close
 			this.innerHTML = "close";
-			gridLayout.style.gridTemplateRows = "60px auto 25%";
+			if (x == 0) {
+				gridLayout.style.gridTemplateRows = "40px auto auto";
+				gridLayout.style.gridTemplateColumns = "25% auto";
+			} else if (x == 1) {
+				//mobile landscape
+				gridLayout.style.gridTemplateRows = "60px auto 25%";
+				gridLayout.style.gridTemplateColumns = "auto 100%";
+			} else if (x == 2) {
+			}
 		}
 	});
 
-	document.querySelectorAll('.showmore').forEach(item => {
-		item.addEventListener('click', event => {
+	document.querySelectorAll(".showmore").forEach((item) => {
+		item.addEventListener("click", (event) => {
 			actionToggle(event);
-		})
-	  })
-  
+		});
+	});
 };
